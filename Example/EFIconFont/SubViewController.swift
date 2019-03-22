fb68134a6c6854b1ecb6942b29982ca0ca83a0be
EyreFree <eyrefree@eyrefree.org>
2019-03-22 22:22:38 +0800

    Update demo

---
+++ Example/EFIconFont/SubViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SubViewController.swift
+//  EFIconFont
+//
+//  Created by EyreFree on 03/19/2019.
+//  Copyright (c) 2019 EyreFree. All rights reserved.
+//
+
+import UIKit
+import EFIconFont
+
+class SubViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    let tableView: UITableView = UITableView()
+
+    let icons: [(key: String, value: String)]
+    let font: UIFont?
+
+    init(title: String, font: UIFont?, dictionary: [String : String]) {
+        self.icons = Array(dictionary)
+        self.font = font
+        super.init(nibName: nil, bundle: nil)
+        self.navigationItem.title = title
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupControls()
+    }
+    
+    func setupControls() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = CGRect(origin: CGPoint.zero, size: UIScreen.main.bounds.size)
+        self.view.addSubview(tableView)
+    }
+
+    // MARK:- UITableView
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reuseIdentifier: String = "Title"
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) ?? UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: reuseIdentifier)
+        cell.detailTextLabel?.font = font
+        cell.textLabel?.text = icons[indexPath.row].key
+        cell.detailTextLabel?.text = icons[indexPath.row].value
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 45
+    }
+}
--- Example/EFIconFont/ViewController.swift
+++ Example/EFIconFont/ViewController.swift
@@ -9,31 +9,78 @@
 import UIKit
 import EFIconFont
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    let tableView: UITableView = UITableView()
+    let iconfonts: [String] = [
+        "AntDesign",
+        "AwesomeBrands",
+        "AwesomeRegular",
+        "AwesomeSolid",
+        "ElusiveIcons",
+        "Ionicons",
+        "Octicons"
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        self.navigationItem.title = "EFIconFont"
         setupControls()
     }
     
     func setupControls() {
-        let colors: [UIColor] = [UIColor.black, UIColor.white, UIColor.blue, UIColor.red, UIColor.green, UIColor.purple, UIColor.orange, UIColor.yellow, UIColor.gray]
-        let textView = UITextView()
-        textView.isEditable = false
-        let content: NSMutableAttributedString = NSMutableAttributedString()
-        for (index, item) in EFIconFont.antDesign.allCases.enumerated() {
-            if let attributedString = item.attributedString(size: 32, foregroundColor: colors[index % colors.count], backgroundColor: colors[(index + 1) % colors.count]) {
-                content.append(attributedString)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = CGRect(origin: CGPoint.zero, size: UIScreen.main.bounds.size)
+        self.view.addSubview(tableView)
+    }
+
+    // MARK:- UITableView
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return iconfonts.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reuseIdentifier: String = "Title"
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) ?? UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: reuseIdentifier)
+        cell.textLabel?.text = iconfonts[indexPath.row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 45
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let items: (font: UIFont?, icons: [String : String]) = {
+            switch indexPath.row {
+            case 0:
+                return (EFIconFont.antDesign.accountBook.font(size: 32), EFIconFont.antDesign.dictionary)
+            case 1:
+                return (EFIconFont.awesomeBrands.accessibleIcon.font(size: 32), EFIconFont.awesomeBrands.dictionary)
+            case 2:
+                return (EFIconFont.awesomeRegular.addressCard.font(size: 32), EFIconFont.awesomeRegular.dictionary)
+            case 3:
+                return (EFIconFont.awesomeSolid.addressBook.font(size: 32), EFIconFont.awesomeSolid.dictionary)
+            case 4:
+                return (EFIconFont.elusiveIcons.addressBook.font(size: 32), EFIconFont.elusiveIcons.dictionary)
+            case 5:
+                return (EFIconFont.ionicons.iosAddCircle.font(size: 32), EFIconFont.ionicons.dictionary)
+            case 6:
+                return (EFIconFont.octicons.arrowDown.font(size: 32), EFIconFont.octicons.dictionary)
+            default:
+                return (UIFont.systemFont(ofSize: 32), [:])
             }
-        }
-        textView.attributedText = content
-        textView.frame = CGRect(origin: CGPoint.zero, size: UIScreen.main.bounds.size)
-        self.view.addSubview(textView)
-        
-        // let imageView = UIImageView()
-        // imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        // imageView.image = EFIconFontFontAwesomeBrands.adobe.image(size: CGSize(width: 100, height: 100), foregroundColor: UIColor.white, backgroundColor: UIColor.green)
-        // self.view.addSubview(imageView)
+        }()
+        let title: String = iconfonts[indexPath.row]
+        let subViewController: SubViewController = SubViewController(title: title, font: items.font, dictionary: items.icons)
+        self.navigationController?.pushViewController(subViewController, animated: true)
     }
 }
